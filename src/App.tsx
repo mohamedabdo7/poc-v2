7f7e654521b0aa25aa8d72f206fe34a6cfe90e1e
mohamedabdo7 <miido.abdo2018@gmail.com>
2026-01-17 14:06:27 +0200

    upt

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
-/**
- * التطبيق الرئيسي
- *
- * نقطة الدخول للمشروع - ببساطة يعرض المشهد
- */
-
+import { Suspense } from "react";
 import { Scene } from "./components/Scene";
-import { preloadOfficeModel } from "./components/OfficeModel";
-
-// مسار موديل المكتب
-// ضع ملف GLTF/GLB في مجلد public/models/
-const OFFICE_MODEL_PATH = "/models/KAFD-Room2.glb";
-
-// تحميل الموديل مسبقاً لتحسين الأداء
-preloadOfficeModel(OFFICE_MODEL_PATH);
 
 function App() {
   return (
     <div className="App">
-      <Scene modelPath={OFFICE_MODEL_PATH} />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Scene modelPath="/models/KAFD-Room3.glb" />
+      </Suspense>
     </div>
   );
 }
 
 export default App;
+
+// /**
+//  * التطبيق الرئيسي
+//  *
+//  * نقطة الدخول للمشروع - ببساطة يعرض المشهد
+//  */
+
+// import { Scene } from "./components/Scene";
+// import { preloadOfficeModel } from "./components/OfficeModel";
+
+// // مسار موديل المكتب
+// // ضع ملف GLTF/GLB في مجلد public/models/
+// const OFFICE_MODEL_PATH = "/models/KAFD-Room2.glb";
+
+// // تحميل الموديل مسبقاً لتحسين الأداء
+// preloadOfficeModel(OFFICE_MODEL_PATH);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Scene modelPath={OFFICE_MODEL_PATH} />
+//     </div>
+//   );
+// }
+
+// export default App;
