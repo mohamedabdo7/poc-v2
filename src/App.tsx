df27387a3dd968e67e945f12fa13defa252d1656
mohamedabdo7 <miido.abdo2018@gmail.com>
2026-01-17 14:03:38 +0200

    upt

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ import { preloadOfficeModel } from "./components/OfficeModel";
 
 // مسار موديل المكتب
 // ضع ملف GLTF/GLB في مجلد public/models/
-const OFFICE_MODEL_PATH = "/models/KAFD-Room3.glb";
+const OFFICE_MODEL_PATH = "/models/KAFD-Room2.glb";
 
 // تحميل الموديل مسبقاً لتحسين الأداء
 preloadOfficeModel(OFFICE_MODEL_PATH);
